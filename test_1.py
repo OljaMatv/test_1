270a21160f7cdf846496462b9d8ef95f83c83801
olja <matvejevaolga@gmail.com>
2023-04-02 20:54:02 +0300

    fifth commit

---
+++ test_1.py
@@ -0,0 +1,7 @@
+def capitalize_all_letters(str):
+    """Capitalizes all letters in the input string."""
+    return str.upper()
+
+def capitalize_first_letters(str):
+    """Capitalizes the first letter of each word in the input string."""
+    return str.title()
